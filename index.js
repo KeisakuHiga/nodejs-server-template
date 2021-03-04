88c4552577d3fd204e870ba10a95a9216b84bad0
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-03-04 23:20:23 +0900

    set up eslint standard style

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// project directory structure
+// https://dev.to/devlcodes/file-structure-of-a-node-project-3opk
+// https://blog.logrocket.com/the-perfect-architecture-flow-for-your-next-node-js-project/
+
+// style guide
+// google: https://google.github.io/styleguide/jsguide.html
+// airbnb: https://github.com/airbnb/javascript
+
 const express = require('express')
 const app = express()
 
