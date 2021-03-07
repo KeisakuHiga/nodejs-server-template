81160c262e1ab60ef8d3af85f2a801d166b4bff4
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-03-07 14:04:55 +0900

    added getmarkets endpoint

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const homeServices = require('../services/homeServices')
 
 router.get('/', homeServices.helloWorldService)
+router.get('/getmarkets', homeServices.getMarketsService)
 
 module.exports = router
--- src/services/homeServices.js
+++ src/services/homeServices.js
@@ -1,8 +1,19 @@
+const axios = require('axios')
+
 module.exports = {
   helloWorldService: (req, res) => {
     const result = {
       greed: 'hello world'
     }
     res.json(result)
+  },
+  getMarketsService: async (req, res) => {
+    try {
+      // invoke bitFlyer's API to get markets info
+      const { data } = await axios.get('https://api.bitflyer.com/v1/getmarkets')
+      res.json(data)
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
