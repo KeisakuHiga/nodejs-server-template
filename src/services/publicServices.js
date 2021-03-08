bf6787f109fdb14b80f44d4019af6c3e9a12ef18
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-03-08 23:30:52 +0900

    trying to connect postgresql

--- src/services/publicServices.js
+++ src/services/publicServices.js
@@ -1,4 +1,15 @@
 const axios = require('axios')
+const { Client } = require('pg')
+
+const client = new Client({
+  user: process.env.DB_USER, // DB のユーザー名を指定
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD, // DB のパスワードを指定
+  port: process.env.DB_PORT
+})
+console.log(client)
+client.connect()
 
 module.exports = {
   /**
@@ -7,6 +18,17 @@ module.exports = {
    * @param {*} res
    */
   markets: async (req, res) => {
+    try {
+      const query = `
+      SELECT *
+        FROM users;
+      `
+      const result = await client.query(query)
+      console.log(result)
+      client.end()
+    } catch (err) {
+      console.log(err)
+    }
     try {
       const { data } = await axios.get('https://api.bitflyer.com/v1/markets')
       res.json(data)
