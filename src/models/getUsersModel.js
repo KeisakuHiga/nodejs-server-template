e1e9819f3e798b0f0d756ef27fd8e6ddabb1ee2e
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-03-12 22:42:49 +0900

    changed logic to get all users

--- src/models/getUsersModel.js
+++ src/models/getUsersModel.js
@@ -1,14 +1,14 @@
-const client = require('../config/db')
-
-const query = `
-      SELECT *
-        FROM users;
-      `
+const { User } = require('../config/db')
 
 module.exports = async () => {
-  const result = await client.query(query)
-  client.end()
-  return {
-    users: result.rows
-  }
+  const users = await User.findAll({
+    attributes: [
+      'user_id',
+      'user_first_name',
+      'user_last_name',
+      'user_age']
+  })
+  console.log(users.every(user => user instanceof User)) // true
+  console.log('All users:', JSON.stringify(users, null, 2))
+  return users
 }
