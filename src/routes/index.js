8005ce1c7b45f7478881c2d28473301fdff52f4f
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-03-13 23:41:24 +0900

    separated routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
-const bitFlyer = require('./bitFlyer')
+const userRoutes = require('./userRoutes')
+const cryptoRoutes = require('./cryptoRoutes')
 
-router.use('/', bitFlyer)
+router.use('/user', userRoutes)
+router.use('/crypto', cryptoRoutes)
 
 module.exports = router
