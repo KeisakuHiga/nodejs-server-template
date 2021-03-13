22b541bc93a80e33af1ba7ec712b263b25dfedb3
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-03-13 23:37:50 +0900

    changed file name

--- src/routes/cryptoRoutes.js
+++ src/routes/cryptoRoutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const publicServices = require('../services/publicServices')
 
-router.get('/getUsers', publicServices.getUsers)
 router.get('/markets', publicServices.markets)
 router.get('/board', publicServices.board)
 router.get('/ticker', publicServices.ticker)
