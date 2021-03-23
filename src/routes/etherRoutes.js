8a2b68ecc9e53240c67670346e465f42ddd5e0dc
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2021-03-23 22:13:56 +0900

    accept body data from request

--- src/routes/etherRoutes.js
+++ src/routes/etherRoutes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router()
 const getUserBalance = require('../services/getUserBalanceService')
-const { provider } = require('../contract/contract')
+// const { provider } = require('../contract/contract')
 
+/**
+ * @param {string} account
+ * @return {number} balance
+ */
 router.get('/getUserBalance', async (req, res) => {
   try {
-    const accounts = await provider.listAccounts()
-    const balance = await getUserBalance(accounts[0])
+    // const accounts = await provider.listAccounts()
+    // const balance = await getUserBalance(accounts[0])
+    const { account } = req.body
+    const balance = await getUserBalance(account)
     res.json(balance)
   } catch (err) {
     console.log(err)
